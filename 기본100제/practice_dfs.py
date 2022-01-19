50a7b5f5bb238aa64ed483f9052ac270c47a9cae
Minjun Cha <62837074+whwhdnfl2@users.noreply.github.com>
2022-01-19 18:30:42 +0900

    dfs, bfs 연습

--- 기본100제/practice_dfs.py
+++ 기본100제/practice_dfs.py
@@ -1,11 +1,24 @@
-def dfs(graph, v, visited):
-   visited[v] = True
-   print(v)
-   for i in graph[v]:
-       if not visited[i]:
-           dfs(graph, i, visited)
+from collections import deque
 
-visited = [False] * 9
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque()
+    queue.append(start)
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end= ' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
 graph = [
     [],
     [2, 3, 8],
@@ -17,5 +30,8 @@ graph = [
     [2, 6, 8],
     [1, 7]
 ]
-
+visited = [False] * 9
 dfs(graph, 1, visited)
+print()
+visited = [False] * 9
+bfs(graph, 1, visited)
