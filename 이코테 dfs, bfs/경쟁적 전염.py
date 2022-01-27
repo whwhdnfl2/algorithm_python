cc35b8b5192a492ba276428ed9f6dedf4a1d14a2
Minjun Cha <62837074+whwhdnfl2@users.noreply.github.com>
2022-01-27 19:52:24 +0900

    경쟁적 전염

--- 이코테 dfs, bfs/경쟁적 전염.py
+++ 이코테 dfs, bfs/경쟁적 전염.py
@@ -6,12 +6,13 @@ for i in range(N):
     data.append(list(map(int, input().split())))
 S, X, Y = map(int, input().split())
 
-queue = deque()
-for k in range(1, K + 1):
-    for i in range(N):
-        for j in range(N):
-            if data[i][j] == k:
-                queue.append([i, j])
+temp = []
+for i in range(N):
+    for j in range(N):
+        if data[i][j] != 0:
+            temp.append([data[i][j], i, j])
+temp.sort()
+queue = deque(temp)
 #print(queue)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -20,8 +21,7 @@ before = 0
 after = 0
 info = 0
 while queue:
-    x, y = queue.popleft()
-    count = data[x][y]
+    count, x, y = queue.popleft()
     after = data[x][y]
     if before == K and after == 1:
         info += 1
@@ -34,6 +34,6 @@ while queue:
         if 0 <= nx < N and 0 <= ny < N:
             if data[nx][ny] == 0:
                 data[nx][ny] = count
-                queue.append([nx, ny])
+                queue.append([data[nx][ny], nx, ny])
     #print(queue)
 print(data[X - 1][Y - 1])
