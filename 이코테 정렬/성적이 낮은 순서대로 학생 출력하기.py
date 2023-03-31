83d8ed8b3299cdbdd45b60d8f8611744a3319c22
Minjun Cha <whwhdnfl2@naver.com>
2023-03-31 09:30:03 +0900

    Delete 이코테 정렬 directory

--- 이코테 정렬/성적이 낮은 순서대로 학생 출력하기.py
+++
@@ -1,8 +0,0 @@
-data = []
-N = int(input())
-for i in range(N):
-    input_data = input().split()
-    data.append((input_data[0], int(input_data[1])))
-
-data.sort(key=lambda x: x[1])
-print(data)
--- 이코테 정렬/위에서 아래로.py
+++
@@ -1,9 +0,0 @@
-data = []
-
-N = int(input())
-for i in range(N):
-    data.append(int(input()))
-
-data.sort(reverse=True)
-for i in data:
-    print(i, end= ' ')
